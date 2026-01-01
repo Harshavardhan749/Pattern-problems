d0ea8066e342cc03f8c1096bf0f83bdadde26cdd
Harshavardhan749 <harshavardhan.459876@gmail.com>
2026-01-01 18:52:06 +0530

    Implement equilateral triangle display with asterisks
    This program prompts the user for the number of rows and displays an equilateral triangle using asterisks. It formats the output to align the asterisks correctly.

---
+++ equilateral_asterisk_triagle.py
@@ -0,0 +1,13 @@
+# Python program to display equilateral triagle using asterisk(*).
+
+r=int(input("Enter number of rows")
+for i in range(1,r+1):
+    print(" "*(r-i)+"* "*i)
+
+"""output:-
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * *
+"""
